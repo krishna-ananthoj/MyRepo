b06a1868e8a1957f5af8764aa801ff19dc3bd242
KrisihnaAnanthoj <krishna.ananthoj@gmail.com>
2014-11-23 22:24:01 +0530

    First Project initial commit

---
+++ JavaMaven1/src/main/java/test/krish/Test1.java
@@ -0,0 +1,14 @@
+package test.krish;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+	
+		String name = "Krishna.Ananthoj";
+		System.out.println("FirstNamae:"+nam e.split("[.]")[0]);
+		System.out.println("LastName:"+name.split("[.]")[1]);
+		System.out.println("Hello");
+
+	}
+
+}
